0f4c96bb8b66d9bb3a45f5309903b0dccc4f114c
xiaoruge <chrjxjwzy@foxmail.com>
2025-07-01 19:12:24 +0800

    Add files via upload

---
+++ static/userfiles/陈厚儒@21194/ai.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+string strip(string str1,char chara)
+{
+	string str2="";
+	for(int i=0;i<str1;i++)
+	{
+		if(str[i]!=chara)str2+=str[i];
+	}
+	return str2;
+}
+int main()
+{
+	string abc="";
+	while(true){
+		getline(cin,abc);
+		cin
+	}
+		
+	return 0;
+}
